588523f909cbb0044470695604067d97ef94d01e
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-01-28 10:09:55 +0900

    done

--- js/libraries/get-quote.js
+++ js/libraries/get-quote.js
@@ -1,7 +1,7 @@
 class GetQuote {
   constructor() {
     this.proxyUrl = 'https://damp-garden-72716.herokuapp.com/';
-    this.apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    this.apiUrl = 'https://type.fit/api/quotes';
     this.quoteText = document.querySelector('#js-quote');
     this.authorName = document.querySelector('#js-author');
     this.twitterBtn = document.querySelector('#js-twitter');
@@ -12,26 +12,34 @@ class GetQuote {
     spinner.start();
     try {
       const response = await fetch(this.proxyUrl + this.apiUrl);
-      const data = await response.json();
-      this._showTweet(data);
+      const apiQuotes = await response.json();
+      const randomQuote = this._newQuote(apiQuotes);
+      this._showTweet(randomQuote);
       spinner.remove();
     } catch (error) {
       console.log(error);
-      getQuote();
+      // getQuote();
     }
   }
 
-  _showTweet(data) {
+  _newQuote(apiQuotes) {
+    // Pick a random quote from apiQuotes array
+    const randomNumber = Math.floor(Math.random() * apiQuotes.length);
+    const randomQuote = apiQuotes[randomNumber];
+    return randomQuote;
+  }
+
+  _showTweet(randomQuote) {
     // If Author is blank, add 'Unknown'
-    data.quoteAuthor = ''
+    randomQuote.author = ''
       ? (this.authorName.textContent = 'Unknown')
-      : (this.authorName.textContent = data.quoteAuthor);
+      : (this.authorName.textContent = randomQuote.author);
 
     // Reduce font size for long quotes
-    data.quoteText.length > 120
+    randomQuote.text.length > 120
       ? this.quoteText.classList.add('long-quote')
       : this.quoteText.classList.remove('long-quote');
 
-    this.quoteText.textContent = data.quoteText;
+    this.quoteText.textContent = randomQuote.text;
   }
 }
